b7a2e8924747e8fd605bc2b88a6717f5dee2f240
xxxDrez <xxxdrez@MacBook-Pro-xxxDrez.local>
2021-08-10 04:35:18 +0300

    Завершенна работа над функциональными возможностями library.js

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+console.log(isArray(['1','2','3']));
+console.log(isDate('1 May 2010'));
+console.log(isNumber(1));
+console.log(isString('Test'));
+console.log(isFunction(function(){}));
+console.log(isBoole(true));
+console.log(isUndefined(undefined));
+console.log(isNull(null));
+console.log(deepEqual(Object,Object));
---
+++ assets/js/library.js
@@ -0,0 +1,59 @@
+
+function isArray(obj){
+    return Array.isArray(obj);
+}
+
+function isDate(obj){
+    var isValid = obj.split(' To ').every(function(s) {
+        return moment(s,'D MMM YYYY').isValid();
+    });
+    return isValid;
+}
+
+function isNumber(obj){
+    return Number.isInteger(obj);
+}
+
+function isString(obj){
+    return typeof obj === 'string';
+}
+
+function isFunction(obj){
+    return typeof obj === 'function';
+}
+
+function isBoole(obj){
+    return typeof obj === 'boolean';
+}
+
+function isUndefined(obj){
+    return typeof(obj) === 'undefined';
+}
+
+function isNull(obj){
+    return typeof(obj) === 'object'
+}
+
+function deepEqual(firstObj, secondObj) {
+    if (firstObj === secondObj) {
+      return true;
+    } else if (isObject(firstObj) && isObject(secondObj)) {
+      if (Object.keys(firstObj).length !== Object.keys(secondObj).length) { return false; }
+      for (var prop in firstObj) {
+        if (!deepEqual(firstObj[prop], secondObj[prop])) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    function isObject(obj) {
+      if (typeof obj === "object" && obj != null) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+
